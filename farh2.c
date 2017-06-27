da92a1e2c68ca39eca24d4ba21b57d1dec037fbb
Mehar Goli <goli.mehar@gmail.com>
2017-06-26 21:23:46 -0700

    updated farenheit for loop  program w/constants

--- farh2.c
+++ farh2.c
@@ -2,13 +2,17 @@
 
 /* converting farenheit to celsius ° C = (5/ 9) (° F − 32) */
 
+#define INITIAL 0
+#define STEP 20
+#define FINAL 300
+
 main ()
 {
   float farh, celsius;
 
   printf("%6s %10s\n", "farh", "celsius");
 
-  for (farh = 0; farh <= 300; farh = farh + 20) {
+  for (farh = INITIAL; farh <= FINAL; farh = farh + STEP) {
     celsius = (5.0/9.0)*(farh - 32.0);
     printf("%6.0f %10.2f\n", farh, celsius);
 
